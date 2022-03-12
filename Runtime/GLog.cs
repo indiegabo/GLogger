836f0235e2450f02cafe819674464cec483c2e5c
Indie Gabo <indiegabodev@gmail.com>
2022-03-12 16:48:01 -0300

    First Try

---
+++ Runtime/GLog.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GLog : GLogger
+{
+    public static GLog I;
+
+    protected virtual void Awake()
+    {
+        I = this;
+    }
+
+    public void Log(string message, Object sender = null)
+    {
+        DoLog(message, color: whiteHEX, sender: sender);
+    }
+
+    public void Success(string message, Object sender = null)
+    {
+        DoLog(message, successHEX, sender);
+    }
+
+    public void Warning(string message, Object sender = null)
+    {
+        DoLog(message, warningHEX, sender);
+    }
+
+    public void Danger(string message, Object sender = null)
+    {
+        DoLog(message, dangerHEX, sender);
+    }
+
+    [MenuItem("GameObject/GLoggers/Main")]
+    public static void CreateSeparator(MenuCommand menuCommand)
+    {
+        GameObject logger = new GameObject("GLog");
+        logger.AddComponent<GLog>();
+        GameObjectUtility.SetParentAndAlign(logger, menuCommand.context as GameObject);
+        Undo.RegisterCreatedObjectUndo(logger, "Create " + logger.name);
+        Selection.activeObject = logger;
+    }
+}
