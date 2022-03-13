250769b7795ae0d398aeb30da7889b1e0e2a6d79
Indie Gabo <indiegabodev@gmail.com>
2022-03-13 09:05:50 -0300

    Comments

--- Runtime/GLog.cs
+++ Runtime/GLog.cs
@@ -3,45 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-namespace IndieGabo.GLogger
+/// <summary>
+/// The default GLogger. 
+/// </summary>
+public class GLog : GLogger
 {
-    public class GLog : GLogger
+
+    /// <summary>
+    /// The GLog instance to be accessed statically
+    /// </summary>    
+    public static GLog I;
+
+    protected virtual void Awake()
     {
-        public static GLog I;
+        I = this; // Defining itself as the instance. Kinda like a Singleton.
+    }
 
-        protected virtual void Awake()
-        {
-            I = this;
-        }
+    /// <summary>
+    /// The GLoggers default Log message
+    /// </summary>
+    /// <param name="message"> The message to be logged </param>
+    /// <param name="sender"> Optional: The object the log message is comming from </param>
+    public void Log(string message, Object sender = null)
+    {
+        DoLog(message, color: whiteHEX, sender: sender);
+    }
 
-        public void Log(string message, Object sender = null)
-        {
-            DoLog(message, color: whiteHEX, sender: sender);
-        }
+    /// <summary>
+    /// The success colored Log message
+    /// </summary>
+    /// <param name="message"> The message to be logged </param>
+    /// <param name="sender"> Optional: The object the log message is comming from </param>
+    public void Success(string message, Object sender = null)
+    {
+        DoLog(message, successHEX, sender);
+    }
 
-        public void Success(string message, Object sender = null)
-        {
-            DoLog(message, successHEX, sender);
-        }
+    /// <summary>
+    /// The warning colored Log message
+    /// </summary>
+    /// <param name="message"> The message to be logged </param>
+    /// <param name="sender"> Optional: The object the log message is comming from </param>
+    public void Warning(string message, Object sender = null)
+    {
+        DoLog(message, warningHEX, sender);
+    }
 
-        public void Warning(string message, Object sender = null)
-        {
-            DoLog(message, warningHEX, sender);
-        }
+    /// <summary>
+    /// The danger colored Log message
+    /// </summary>
+    /// <param name="message"> The message to be logged </param>
+    /// <param name="sender"> Optional: The object the log message is comming from </param>
+    public void Danger(string message, Object sender = null)
+    {
+        DoLog(message, dangerHEX, sender);
+    }
 
-        public void Danger(string message, Object sender = null)
-        {
-            DoLog(message, dangerHEX, sender);
-        }
-
-        [MenuItem("GameObject/GLogger/GLog")]
-        public static void CreateSeparator(MenuCommand menuCommand)
-        {
-            GameObject logger = new GameObject("GLog");
-            logger.AddComponent<GLog>();
-            GameObjectUtility.SetParentAndAlign(logger, menuCommand.context as GameObject);
-            Undo.RegisterCreatedObjectUndo(logger, "Create " + logger.name);
-            Selection.activeObject = logger;
-        }
+    [MenuItem("GameObject/GLogger/GLog")]
+    public static void CreateSeparator(MenuCommand menuCommand)
+    {
+        GameObject logger = new GameObject("GLog");
+        logger.AddComponent<GLog>();
+        GameObjectUtility.SetParentAndAlign(logger, menuCommand.context as GameObject);
+        Undo.RegisterCreatedObjectUndo(logger, "Create " + logger.name);
+        Selection.activeObject = logger;
     }
 }
--- Runtime/GLogger.cs
+++ Runtime/GLogger.cs
@@ -2,36 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace IndieGabo.GLogger
+/// <summary>
+/// Extend from this class to create your custom GLogger
+/// and use its utillities
+/// </summary>
+public abstract class GLogger : MonoBehaviour
 {
-    public abstract class GLogger : MonoBehaviour
+    [Header("Settings")]
+    [SerializeField] protected bool m_shouldLog = true;
+
+    [Header("Colors")]
+    [SerializeField] protected Color m_successColor = Color.green; // Green
+    [SerializeField] protected Color m_warningColor = Color.yellow; // Yellow
+    [SerializeField] protected Color m_dangerColor = Color.red; // Red
+
+    /// <summary>
+    /// The success color's Hexadecimal code
+    /// </summary>
+    /// <returns> The hex code string </returns>
+    protected string successHEX => "#" + ColorUtility.ToHtmlStringRGB(m_successColor);
+
+    /// <summary>
+    /// The warning color's Hexadecimal code
+    /// </summary>
+    /// <returns> The hex code string </returns>
+    protected string warningHEX => "#" + ColorUtility.ToHtmlStringRGB(m_warningColor);
+
+    /// <summary>
+    /// The danger color's Hexadecimal code
+    /// </summary>
+    /// <returns> The hex code string </returns>
+    protected string dangerHEX => "#" + ColorUtility.ToHtmlStringRGB(m_dangerColor);
+
+    /// <summary>
+    /// The white color's Hexadecimal code
+    /// </summary>
+    /// <returns> The hex code string </returns>
+    protected string whiteHEX => "#FFFFFF";
+
+    /// <summary>
+    /// The GLogger's root method for logging. Use this if you are creating a custom 
+    /// GLogger.
+    /// </summary>
+    /// <param name="message">The message to be logged</param>
+    /// <param name="color">The hex code of the color your logged text shoul have</param>
+    /// <param name="sender">The object where the log is comming from if any</param>
+    protected virtual void DoLog(string message, string color = "#FFFFFF", Object sender = null)
     {
-        [Header("Settings")]
-        [SerializeField] protected bool m_shouldLog = true;
+        if (!m_shouldLog) return;
 
-        [Header("Colors")]
-        [SerializeField] protected Color m_successColor = Color.green; // Green
-        [SerializeField] protected Color m_warningColor = Color.yellow; // Yellow
-        [SerializeField] protected Color m_dangerColor = Color.red; // Red
-
-        protected string successHEX => "#" + ColorUtility.ToHtmlStringRGB(m_successColor);
-        protected string warningHEX => "#" + ColorUtility.ToHtmlStringRGB(m_warningColor);
-        protected string dangerHEX => "#" + ColorUtility.ToHtmlStringRGB(m_dangerColor);
-        protected string whiteHEX => "#FFFFFF";
-
-        protected virtual void DoLog(string message, string color = "#FFFFFF", Object sender = null)
+        if (sender != null)
         {
-            if (!m_shouldLog) return;
+            Debug.Log($"<color={color}> {message} </color>", sender);
 
-            if (sender != null)
-            {
-                Debug.Log($"<color={color}> {message} </color>", sender);
-
-            }
-            else
-            {
-                Debug.Log($"<color={color}> {message} </color>");
-            }
+        }
+        else
+        {
+            Debug.Log($"<color={color}> {message} </color>");
         }
     }
 }
