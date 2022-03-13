ce8b982f85fe99e56b1446bdac11b0539fa3a185
Indie Gabo <indiegabodev@gmail.com>
2022-03-12 22:39:21 -0300

    Tessting without namespaces

--- Runtime/GLog.cs
+++ Runtime/GLog.cs
@@ -3,45 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-namespace IndieGabo.GLogger
+public class GLog : GLogger
 {
-    public class GLog : GLogger
+    public static GLog I;
+
+    protected virtual void Awake()
     {
-        public static GLog I;
+        I = this;
+    }
 
-        protected virtual void Awake()
-        {
-            I = this;
-        }
+    public void Log(string message, Object sender = null)
+    {
+        DoLog(message, color: whiteHEX, sender: sender);
+    }
 
-        public void Log(string message, Object sender = null)
-        {
-            DoLog(message, color: whiteHEX, sender: sender);
-        }
+    public void Success(string message, Object sender = null)
+    {
+        DoLog(message, successHEX, sender);
+    }
 
-        public void Success(string message, Object sender = null)
-        {
-            DoLog(message, successHEX, sender);
-        }
+    public void Warning(string message, Object sender = null)
+    {
+        DoLog(message, warningHEX, sender);
+    }
 
-        public void Warning(string message, Object sender = null)
-        {
-            DoLog(message, warningHEX, sender);
-        }
+    public void Danger(string message, Object sender = null)
+    {
+        DoLog(message, dangerHEX, sender);
+    }
 
-        public void Danger(string message, Object sender = null)
-        {
-            DoLog(message, dangerHEX, sender);
-        }
-
-        [MenuItem("GameObject/GLogger/GLog")]
-        public static void CreateSeparator(MenuCommand menuCommand)
-        {
-            GameObject logger = new GameObject("GLog");
-            logger.AddComponent<GLog>();
-            GameObjectUtility.SetParentAndAlign(logger, menuCommand.context as GameObject);
-            Undo.RegisterCreatedObjectUndo(logger, "Create " + logger.name);
-            Selection.activeObject = logger;
-        }
+    [MenuItem("GameObject/GLogger/GLog")]
+    public static void CreateSeparator(MenuCommand menuCommand)
+    {
+        GameObject logger = new GameObject("GLog");
+        logger.AddComponent<GLog>();
+        GameObjectUtility.SetParentAndAlign(logger, menuCommand.context as GameObject);
+        Undo.RegisterCreatedObjectUndo(logger, "Create " + logger.name);
+        Selection.activeObject = logger;
     }
 }
--- Runtime/GLogger.cs
+++ Runtime/GLogger.cs
@@ -2,36 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace IndieGabo.GLogger
+public abstract class GLogger : MonoBehaviour
 {
-    public abstract class GLogger : MonoBehaviour
+    [Header("Settings")]
+    [SerializeField] protected bool m_shouldLog = true;
+
+    [Header("Colors")]
+    [SerializeField] protected Color m_successColor = Color.green; // Green
+    [SerializeField] protected Color m_warningColor = Color.yellow; // Yellow
+    [SerializeField] protected Color m_dangerColor = Color.red; // Red
+
+    protected string successHEX => "#" + ColorUtility.ToHtmlStringRGB(m_successColor);
+    protected string warningHEX => "#" + ColorUtility.ToHtmlStringRGB(m_warningColor);
+    protected string dangerHEX => "#" + ColorUtility.ToHtmlStringRGB(m_dangerColor);
+    protected string whiteHEX => "#FFFFFF";
+
+    protected virtual void DoLog(string message, string color = "#FFFFFF", Object sender = null)
     {
-        [Header("Settings")]
-        [SerializeField] protected bool m_shouldLog = true;
+        if (!m_shouldLog) return;
 
-        [Header("Colors")]
-        [SerializeField] protected Color m_successColor = Color.green; // Green
-        [SerializeField] protected Color m_warningColor = Color.yellow; // Yellow
-        [SerializeField] protected Color m_dangerColor = Color.red; // Red
-
-        protected string successHEX => "#" + ColorUtility.ToHtmlStringRGB(m_successColor);
-        protected string warningHEX => "#" + ColorUtility.ToHtmlStringRGB(m_warningColor);
-        protected string dangerHEX => "#" + ColorUtility.ToHtmlStringRGB(m_dangerColor);
-        protected string whiteHEX => "#FFFFFF";
-
-        protected virtual void DoLog(string message, string color = "#FFFFFF", Object sender = null)
+        if (sender != null)
         {
-            if (!m_shouldLog) return;
+            Debug.Log($"<color={color}> {message} </color>", sender);
 
-            if (sender != null)
-            {
-                Debug.Log($"<color={color}> {message} </color>", sender);
-
-            }
-            else
-            {
-                Debug.Log($"<color={color}> {message} </color>");
-            }
+        }
+        else
+        {
+            Debug.Log($"<color={color}> {message} </color>");
         }
     }
 }
